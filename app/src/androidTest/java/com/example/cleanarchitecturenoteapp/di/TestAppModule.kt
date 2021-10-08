ed3949ac12353f260aeb922cc96f13431ab24212
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-10-08 12:02:15 -0600

    Integation UI Test

---
+++ app/src/androidTest/java/com/example/cleanarchitecturenoteapp/di/TestAppModule.kt
@@ -0,0 +1,45 @@
+package com.example.cleanarchitecturenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
+import com.example.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.example.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.example.cleanarchitecturenoteapp.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun providerNoteDatabase(app: Application): NoteDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            NoteDatabase::class.java,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotesUseCase = GetNotesUseCase(repository),
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
+        )
+    }
+
+}
